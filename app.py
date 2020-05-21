88fccb44fdc11664991e0901027fd841c5c349c1
Jemma <jher5522@uni.sydney.edu.au>
2020-05-21 16:26:07 +1000

    refactor

--- app.py
+++ app.py
@@ -19,14 +19,6 @@ app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 
-@app.route('/')
-@auth.login_required
-def search_bar():
-	cur, conn = get_db()
-	all_images = cur.execute(f"SELECT image_id, image_path FROM Images").fetchall()
-	return render_template("home.html", images=all_images) 
-
-
 
 @app.route('/image/<int:image_id>', methods=["GET", "DELETE"])
 @app.route('/image', methods=["POST"])
@@ -35,24 +27,34 @@ def image(image_id=None):
 	cur, conn = get_db()
 	
 	if request.method == "GET":
-		result = cur.execute(
-			f"""SELECT image_id, image_path, contains_pii 
-			FROM Images 
-			WHERE image_id={image_id}
-			AND NOT images.deleted""").fetchone()
-		if result is None:
+		get_image_query = """SELECT image_id, image_path, contains_pii 
+						FROM Images 
+						WHERE image_id=:image_id
+						AND NOT images.deleted"""
+		row = cur.execute(get_image_query, {'image_id': image_id}).fetchone()
+		
+		# check that it found the requested image
+		if row is None:
 			return "Image not found", STATUS_NOT_FOUND
 
-		image_id, image_path, contains_pii = result
-		return json.dumps({'image_id': image_id, 'image_path': image_path, 'contains_pii': contains_pii}), STATUS_OK
+		# return the image data and success
+		return json.dumps(dict(row)), STATUS_OK
 	
 
 	elif request.method ==  "DELETE":
-		r = cur.execute(f"UPDATE Images SET deleted=1 WHERE image_id={image_id}")
-		if r.rowcount ==0:
+		delete_image_query = """
+			UPDATE Images 
+			SET deleted=1 
+			WHERE image_id=:image_id
+			"""
+		r = cur.execute(delete_image_query, {'image_id': image_id})
+		
+		# check that it deleted something
+		if r.rowcount != 1:
 			return "Nothing to delete", STATUS_NOT_FOUND
-		cur.execute(f"""INSERT INTO Logs (object, updated_by, method, image_id, modified_at) 
-			VALUES ('Image', '{g.user}', 'DELETE', {int(image_id)}, datetime('now', 'localtime'))""")
+		
+		# log the deletion
+		log_success = add_log('Image', 'DELETE', image_id, None)
 		conn.commit()
 		return "deleted", STATUS_OK
 
@@ -72,23 +74,25 @@ def image(image_id=None):
 		if not os.path.exists(provided_path.absolute()):
 			return "Image does not exist at specified path", STATUS_BAD_REQUEST
 
-		# check whether it contains ppi
+		# check whether it contains pii
 		contains_pii = check_for_pii(provided_path)
 
 		# insert row
 		rel_image_path = str(provided_path.relative_to(images_dir))
-		r = cur.execute(f"""INSERT INTO Images (image_path, deleted, contains_pii) 
-			VALUES ('{image_path}', 0, {contains_pii})""")
+		insert_image_query = """
+			INSERT INTO Images (image_path, deleted, contains_pii) 
+			VALUES (':image_path', 0, :contains_pii)
+			"""
+		r = cur.execute(insert_image_query, {'image_path': rel_image_path, 'contains_pii': contains_pii})
 
 		# check it worked
-		if r.rowcount == 0:
+		if r.rowcount != 1:
 			return "Problem inserting new image", STATUS_INTERNAL_ERROR
-
 		image_id = cur.execute(f"SELECT last_insert_rowid() FROM Images").fetchone()[0]
-		cur.execute(f"""INSERT INTO Logs (object, updated_by, method, image_id, modified_at) 
-			VALUES ('Image', '{g.user}', 'INSERTION', {int(image_id)}, datetime('now', 'localtime'))""")
-		conn.commit()
 
+		# log the update
+		add_log('Image', 'INSERTION', int(image_id), None)
+		conn.commit()
 		return json.dumps({'image_id': image_id}), STATUS_OK
 
 
@@ -99,39 +103,36 @@ def label(label_id=None):
 	cur, conn = get_db()
 
 	if request.method == "GET":
-		result = cur.execute(f"""
-			SELECT image_id, image_path, first_name, last_name, class_id, geometry
+		get_label_query = """
+			SELECT image_id, label_id, image_path, first_name, last_name, class_id, geometry
 			FROM Labels 
 			JOIN Images USING(image_id) 
 			JOIN Users ON labels.labelled_by = users.username 
-			WHERE label_id={label_id}
-			AND not labels.deleted""").fetchone()
-		if result is None:
+			WHERE label_id=:label_id
+			AND not labels.deleted
+			"""
+
+		row = cur.execute(get_label_query, {'label_id': label_id}).fetchone()
+		if row is None:
 			return "Label not found", STATUS_NOT_FOUND
 
-		image_id, image_path, first_name, last_name, class_id, geometry = result
-
-		return json.dumps(
-			dict( 
-				label_id=label_id,
-				image_id=image_id, 
-				image_path=image_path, 
-				first_name=first_name, 
-				last_name=last_name, 
-				class_id=class_id, 
-				geometry=geometry
-				)
-			)
+		return json.dumps(dict(row))
 
 
 	elif request.method=='DELETE':
-		cur, conn = get_db()
-		r = cur.execute(f"UPDATE Labels SET deleted=1 WHERE label_id={label_id}")
-		if r.rowcount == 0:
+		delete_label_query = """
+			UPDATE Labels 
+			SET deleted=1 
+			WHERE label_id=:label_id
+			"""
+		r = cur.execute(delete_label_query, {'label_id': label_id})
+
+		# check something was deleted
+		if r.rowcount != 1:
 			return "Nothing to delete", STATUS_NOT_FOUND
-		cur.execute(f"""INSERT INTO Logs (object, updated_by, method, label_id, modified_at) 
-			VALUES ('Label', '{g.user}', 'DELETE', {label_id}, datetime('now', 'localtime'))""")
-		print("label deleted")
+
+		# log and return success
+		add_log('Label', 'DELETE', None, int(label_id))
 		conn.commit()
 		return "Label deleted", STATUS_OK
 
@@ -145,54 +146,86 @@ def label(label_id=None):
 		except shapely.errors.WKTReadingError:
 			return "Geometry is not a valid wkt", STATUS_BAD_REQUEST
 
-		r = cur.execute(
-			f"""INSERT INTO Labels (image_id, labelled_by, class_id, geometry, deleted) 
-				VALUES ({int(data['image_id'])}, '{g.user}', {int(data['class_id'])}, '{data['geometry']}', 0)"""
-			)
-		if r.rowcount == 0:
+		# insert new label
+		insert_label_query = """
+			INSERT INTO Labels (image_id, labelled_by, class_id, geometry, deleted) 
+			VALUES (:image_id, :usr, :class_id, :geom, 0)
+			"""
+		query_data = {
+			'image_id': int(data['image_id']), 
+			'usr': g.user,
+			'class_id': int(data['class_id']),
+			'geom': data['geometry']
+			}
+		r = cur.execute(insert_label_query, query_data)
+		
+		# ensure something was inserted
+		if r.rowcount != 1:
 			return "Problem inserting new label", STATUS_INTERNAL_ERROR
-
 		label_id = cur.execute(f"SELECT last_insert_rowid() FROM Labels").fetchone()[0]
-		cur.execute(f"""INSERT INTO Logs (object, updated_by, method, label_id, modified_at) 
-			VALUES ('Label', '{g.user}', 'INSERTION', {int(label_id)}, datetime('now', 'localtime'))""")
-
+		
+		# log and return success
+		add_log('Label', 'INSERTION', None, int(label_id))
 		conn.commit()
-
 		return json.dumps({'label_id': label_id}), STATUS_OK
 
 	elif request.method=='PUT':
 		data = request.values.to_dict()
 
-		# change class_id if its specified
-		update_string=''
-		if 'class_id' in data.keys():
-			update_string += f"class_id={int(data['class_id'])}"
-		if 'geometry' in data.keys():
-			update_string += f" geometry='{data['geometry']}'"
-
-		r = cur.execute(f"UPDATE Labels SET {update_string} WHERE label_id={label_id}")
-		if r.rowcount == 0:
+		# change class_id and/or geometry if its specified
+		update_label_query = f"""
+			UPDATE Labels
+			SET 
+			{'class_id=:class_id' if 'class_id' in data.keys() else ''}
+			{'geometry=:geometry' if 'geometry' in data.keys() else ''}
+			WHERE label_id=:label_id
+			"""
+		r = cur.execute(update_label_query, {'label_id': label_id, **data})
+		
+		# ensure something was updated
+		if r.rowcount != 1:
 			return "Nothing to update", STATUS_NOT_FOUND
-		cur.execute(f"""INSERT INTO Logs (object, updated_by, method, label_id, modified_at) 
-			VALUES ('Label', '{g.user}', 'UPDATE', {int(label_id)}, datetime('now', 'localtime'))""")
+
+		# log and return success
+		add_log('Label', 'UPDATE', None, int(label_id))
 		conn.commit()
 		return "Label updated", STATUS_OK
 
 
+def add_log(obj, method, image_id, label_id):
+	cur, conn = get_db()
+
+	insert_log_query = """
+		INSERT INTO Logs (object, updated_by, method, image_id, label_id, modified_at) 
+		VALUES (:obj, :usr, :method, :im_id, :lab_id, datetime('now', 'localtime'))
+		"""
+	log_data = {
+		'obj': obj,
+		'usr': g.user, 
+		'method': method, 
+		'im_id': image_id, 
+		'lab_id': label_id
+		}
+	r = cur.execute(insert_log_query, log_data)
+	
+	# ensure it logged properly. Otherwise raise error, will which return 500
+	if r.rowcount != 1:
+		raise ValueError('Logging was unsucessful')
+
+
 def get_db():
-	"""Opens a new database connection if there is none yet for the
-	current application context.
-	"""
 	db_path = str(Path(__file__).absolute().parent.joinpath('image_db', 'test_db.sqlite'))
 	if not hasattr(g, 'conn'):
+		# only open new connection if it doesn't already exist. Otherwise return existing
 		g.conn = sqlite3.connect(db_path)
+		g.conn.row_factory = sqlite3.Row
 		g.cur = g.conn.cursor()
 	return g.cur, g.conn
 
 
 @app.teardown_appcontext
 def cleanup(error):
-	"""Closes the database again at the end of the request."""
+	# close db connection and clear g variables
 	if hasattr(g, 'conn'):
 		g.conn.close()
 	g.pop('conn', None)
--- identify_pii.py
+++ identify_pii.py
@@ -2,25 +2,33 @@ from PIL import Image
 import pytesseract
 import string
 
-suspicious_words = set(['name', 'dob', 'd.o.b.'])
-with open('names.txt') as f:
+# list of suspicious words. 
+suspicious_words = set(['name', 'dob', 'd.o.b.', 'address']) # headers for ppi
+with open('names.txt') as f:  # common names
 	names = set([line.rstrip('\n').lower() for line in f])
 suspicious_words.update(names)
 
 def _extract_words(im_path):
+	# get words from image using tesseract
 	im = Image.open(im_path)
 	text = pytesseract.image_to_string(im)
+
+	# remove punctutation and split into set of words
 	clean_text = text.lower().translate(str.maketrans('', '', string.punctuation))
 	words = set(clean_text.split())
 	return words
 
 def check_for_pii(im_path):
+	# check if there are any words on the image
 	words = _extract_words(im_path)
 
 	if words is None:
+		# if there are no words, its fine
 		return False
 
 	if (words & suspicious_words):
+		# if there are any suspicious words report it
 		return True
 
+	# if none of the words are suspicious, its fine
 	return False
